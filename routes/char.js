1b348daea7ce6c4f256a9b3f3c7d65f9a1dac0b1
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-12-01 19:11:53 -0800

    [layout,char route]] fix nav bar and added icons. Also initial post route is done

--- routes/char.js
+++ routes/char.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const axios = require('axios')
 
 router.get('/create', (req, res) => {
-    async function getClassInfo() {
-        let classInfo =  await axios.get('https://api.open5e.com/classes/') 
-        let classResults = classInfo.data.results
-    }
+    axios.get('https://api.open5e.com/classes/') 
+    .then(response => {
+    res.render("char/create", {classInformation: response.data.results})
+    })
 })
 
 module.exports = router;
