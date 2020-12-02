d9a817162f15dfff2b45f59b10431c8df1b52e9b
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-12-02 13:20:50 -0800

    [project] routes dont currently work with how I have my code set up. This is a backup commit in case my next endeavor to throw all the data into tables doesn't go well

--- routes/char.js
+++ routes/char.js
@@ -2,12 +2,27 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 const axios = require('axios')
+const bodyParser =require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 router.get('/create', (req, res) => {
-    axios.get('https://api.open5e.com/classes/') 
+    axios.all([axios.get('https://api.open5e.com/classes/'),
+               axios.get('https://api.open5e.com/races/')])
+    .then(axios.spread((classInfo, raceInfo) => { 
+        // console.log(Object.keys(classInfo.data.results[0]))
+    res.render('char/create', {classInformation: classInfo.data.results, raceInfo: raceInfo.data.results})
+    }))
+})
+
+router.post('/create', urlencodedParser, async (req, res) => {
+    let className = req.body.class.toLowerCase();
+    await axios.get(`https://api.open5e.com/classes/${className}`)
     .then(response => {
-    res.render("char/create", {classInformation: response.data.results})
+    console.log(response.data)
+    res.render('char/create',{classInformation: response.data} )
     })
 })
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const passport = require('./config/ppConfig');
 const flash = require('connect-flash');
 const SECRET_SESSION = process.env.SECRET_SESSION;
 const app = express();
-const kwesforms = require('kwesforms');
+
 // isLoggedIn middleware
 const isLoggedIn = require('./middleware/isLoggedIn');
 app.set('view engine', 'ejs');
