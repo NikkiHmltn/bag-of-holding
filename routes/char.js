35f8b8af88c77c92a6246a80c600117187d92cfb
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-12-03 19:18:25 -0800

    [char.js] ability scores pregenerate for display successfully for standard play

--- routes/char.js
+++ routes/char.js
@@ -28,46 +28,64 @@ router.get('/create', async (req, res) => {
                 let values = dieOne[i].value
                 valueDieOne.push(values)
             }
-            console.log(valueDieOne)
+            let minOne = Math.min(...valueDieOne);
+            let newValueOne = valueDieOne.filter(e => e != minOne);
+            let sumValueOne = newValueOne.reduce((a, b) => a + b, 0)
+                console.log(sumValueOne, "one")
+            
             let dieTwo = diceTwo.data.dice;
             let valueDieTwo = [];
             for(let i = 0;i <  dieTwo.length; i++){
                 let values = dieTwo[i].value
                 valueDieTwo.push(values)
             }
-            console.log(valueDieTwo)
+            let minTwo = Math.min(...valueDieTwo);
+            let newValueTwo = valueDieTwo.filter(e => e != minTwo);
+            let sumValueTwo = newValueTwo.reduce((a, b) => a + b, 0)
+                console.log(sumValueTwo, "two")
+
             let dieThree = diceThree.data.dice;
             let valueDieThree = [];
             for(let i = 0;i <  dieThree.length; i++){
                 let values = dieThree[i].value
                 valueDieThree.push(values)
             }
-            console.log(valueDieThree)
+            let minThree = Math.min(...valueDieThree);
+            let newValueThree = valueDieThree.filter(e => e != minThree);
+            let sumValueThree = newValueThree.reduce((a, b) => a + b, 0)
+                console.log(sumValueThree, "three")
+            
             let dieFour = diceFour.data.dice;
             let valueDieFour = [];
             for(let i = 0;i <  dieFour.length; i++){
                 let values = dieFour[i].value
                 valueDieFour.push(values)
             }
-            console.log(valueDieFour)
+            let minFour = Math.min(...valueDieFour);
+            let newValueFour = valueDieFour.filter(e => e != minFour);
+            let sumValueFour = newValueFour.reduce((a, b) => a + b, 0)
+                console.log(sumValueFour, "four")
+            
             let dieFive = diceFive.data.dice;
             let valueDieFive = [];
             for(let i = 0;i <  dieFive.length; i++){
                 let values = dieFive[i].value
                 valueDieFive.push(values)
             }
-            console.log(valueDieFive)
+            let minFive = Math.min(...valueDieFive);
+            let newValueFive = valueDieFive.filter(e => e != minFive);
+            let sumValueFive = newValueFive.reduce((a, b) => a + b, 0)
+                console.log(sumValueFive, "five")
             let dieSix = diceSix.data.dice;
             let valueDiceSix = [];
             for(let i = 0;i <  dieSix.length; i++){
                 let values = dieSix[i].value
                 valueDiceSix.push(values)
             }
-            console.log(valueDiceSix)
-
-            let min = Math.min(...valueDieOne);
-            let newValue = valueDieOne.filter(e => e != min);
-            console.log(newValue)
+            let minSix = Math.min(...valueDiceSix);
+            let newValueSix = valueDiceSix.filter(e => e != minSix);
+            let sumValueSix = newValueSix.reduce((a, b) => a + b, 0)
+                console.log(sumValueSix, "six")
             db.classes.findAll().then(classes => {
                 let allClasses = classes;
                 objInfo.allClasses = classes;
